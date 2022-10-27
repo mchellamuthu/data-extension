1eb92fb99419f321402b0474707905e437b13ee0
Chella <mchellamuthu@live.com>
2022-10-27 14:04:01 +0530

    added success overlay

--- js/popup.js
+++ js/popup.js
@@ -9,4 +9,19 @@ window.onload = function () {
   projects.addEventListener('change', function () {
     document.getElementById('milestone').disabled = false;
   });
+
+  let toggleDescription = document.getElementById('toggleDescription');
+  toggleDescription.addEventListener('click',function(){
+    document.getElementById('descriptionBox').classList.toggle('hidden');
+    document.getElementById('description').toggleAttribute('disabled');
+  });
+
+  let form = document.getElementById('task_form');
+  form.addEventListener('submit',function(){
+    document.getElementById("overlay").style.display = "block";
+  });
+  let overlay = document.getElementById('overlay');
+  overlay.addEventListener('click',function(){
+    overlay.style.display = "none";
+  });
 }
